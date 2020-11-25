c0875b0f919da5cfa20e395168e2e8a53545796e
James DeSousa <james.desousa45@gmail.com>
2020-11-25 21:40:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = 500 
   min_name = ""
-  name_hash.each do |key,value|
-    min = 500
-    if name_hash[key][value] < min 
-      min = name_hash[key]
-      min_name = key 
+  name_hash.min do |key,value|
+     
     end 
   end 
   min_name
