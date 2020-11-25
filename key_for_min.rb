2245d15f10968ce27380a3b76e6e8c2ec11e0fb4
James DeSousa <james.desousa45@gmail.com>
2020-11-25 21:59:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,16 +4,17 @@
 def key_for_min_value(name_hash)
   min = 500 
   min_name = ""
+if !name_hash.count
+  return nil 
+else 
   name_hash.each do |key,value|
-  if !key && !value 
-    return nil 
-  end 
     if value < min  
       min = value 
       min_name = key 
     end 
      
   end 
+end 
   min_name
 
 end
